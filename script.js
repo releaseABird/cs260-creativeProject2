cb2c3be94086917b0fbbfdb015c0ab71e9e6998b
Lisa Bird <misademoss24@gmail.com>
2022-02-19 16:03:19 -0800

    Got the color changing boxes to work, but am still working on formatting

--- script.js
+++ script.js
@@ -14,17 +14,41 @@ http.onreadystatechange = function() {
 	if(http.readyState == 4 && http.status == 200) {
 		var palette = JSON.parse(http.responseText).result;
     console.log(palette);
-    let results = "";
-    results += "<div class='color-boxes'>";
+    //let results = "";
+    document.getElementById("colorResults").innerHTML = "<div class='color-boxes'>";
+    currColor="";
     for (let i = 0; i < 5; i++){
+      console.log(palette[i]);
+      result = ""
+      result += "<div class='color-box'> <div class='img' id='color" + i + "'></div>";
+      currColor = "#" + palette[i][0].toString(16) + palette[i][1].toString(16) + palette[i][2].toString(16)
+      result += "<p>Color: " + currColor + "</p>";
+      result += "</div>";
+      document.getElementById("colorResults").innerHTML += result;
+      //document.getElementById("colorResults").innerHTML += results;
 
+      console.log(document.getElementById('color' + i));
+
+      //console.log(document.getElementById("color"+ i))
+      document.getElementById('color'+ i).style.backgroundColor = currColor;
+      document.getElementById('color'+ i).style.height = "100px";
+      document.getElementById('color'+ i).style.width = "100px";
+      //document.getElementById("color" + i).style.color = "#" + palette[i][0] + palette[i][1] + palette[i][2];
     }
-    results += "/div";
+
+
+
+    document.getElementById("colorResults").innerHTML += "</div>";
+
+    //console.log(results);
+    //document.getElementById("colorResults").innerHTML = results;
 	}
 }
 
 http.open("POST", url, true);
 http.send(JSON.stringify(data));
+
+
 
 
 
